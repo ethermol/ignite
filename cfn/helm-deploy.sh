82ff24d0f0ade0d9055165d7d3eae18bca595cfa
Peter Mostert <pmostert@mirabeau.nl>
2020-12-17 18:16:49 +0100

    Rebuilding in Ireland, multiple changes

--- cfn/helm-deploy.sh
+++ cfn/helm-deploy.sh
@@ -5,7 +5,8 @@ helm repo add stable https://charts.helm.sh/stable
 helm repo add traefik https://helm.traefik.io/traefik
 
 # Get traefik charts from github
-curl -LO https://github.com/traefik/traefik-helm-chart/archive/master.zip
-unzip master.zip && rm master.zip
-helm install traefik ./traefik-helm-chart-master/traefik
-rm -rf ./traefik-helm-chart-master
+# curl -LO https://github.com/traefik/traefik-helm-chart/archive/master.zip
+# unzip master.zip && rm master.zip
+helm install traefik ./helm-charts/traefik
+kubectl apply -f ./helm-charts/whoami 
+# rm -rf ./traefik-helm-chart-master
--- cfn/helm-install.sh
+++ cfn/helm-install.sh
@@ -2,7 +2,7 @@
 
 [ -f /usr/local/bin/helm ] || \
   { curl -LO https://get.helm.sh/helm-v3.4.1-linux-arm64.tar.gz && \
-    tar xf helm-v3.4.1-linux-arm64.tar.gz && mv linux-arm64/helm /usr/local/bin/ && \
-    rm helm-v3.4.1.*.gz && rm -rf linux-arm64 \
+    tar xf helm-v3.4.1-linux-arm64.tar.gz && sudo mv linux-arm64/helm /usr/local/bin/ && \
+    rm helm-v3.4.1-*.gz && rm -rf linux-arm64 && \
+    :
   }
-
